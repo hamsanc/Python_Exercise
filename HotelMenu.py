c0ab8371aa715c1f23507fdeb2d392a9e9fd302c
hamsanc <72121173+hamsanc@users.noreply.github.com>
2020-11-03 20:55:10 +0530

    Add files via upload
    Hotel Menu Implementation

---
+++ HotelMenu.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  3 20:52:32 2020
+
+@author: hamcy
+"""
+
+#Provided problem:
+x=menu()
+x.add(“idly”,10)
+x.add(“vada”,20)
+x.show()

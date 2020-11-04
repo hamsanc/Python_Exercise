f8aea08360d03a0b426088edb22751f435712ef8
hamsanc <72121173+hamsanc@users.noreply.github.com>
2020-11-04 12:22:32 +0530

    Add files via upload
    implemented the hotel menu - menu class with add and show methods

--- HotelMenu.py
+++ HotelMenu.py
@@ -6,8 +6,20 @@ Created on Tue Nov  3 20:52:32 2020
 @author: hamcy
 """
 
-#Provided problem:
+class menu():
+    def __init__(self):
+        self.menu = dict()    # creates a new empty dict for each dog
+
+    def add(self, key,value):
+        self.menu[key] = value
+        
+    def show(self):
+        print('Hamsa Prasanna Hotel Menu:')
+        for k in self.menu:
+            print('1 Plate ',k,' - ',self.menu[k],'Rupees')
+    
+    
 x=menu()
-x.add(“idly”,10)
-x.add(“vada”,20)
+x.add("idly",10)
+x.add("vada",20)
 x.show()
